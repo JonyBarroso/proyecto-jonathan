093ddfbad373cc1ecdebcc54b56fd27194dcbe94
jonathan.barroso <jony.barroso.2@gmail.com>
2022-06-30 20:42:50 -0300

    se modifico index.html y se puso en uso para realizar la tarea mi archivo.js

---
+++ miarchivo.js
@@ -0,0 +1,9 @@
+let usuario = prompt("Ingrese su usuario Athena");
+
+while(usuario !=  "jonathan.athena"){
+    alert("usuario incorrecto")
+    
+ usuario = prompt("ingrese su usuario athena")   
+}
+
+alert("bienvenido a Athena.Deco")
