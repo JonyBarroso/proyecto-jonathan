f18fd691699097b0020678c34c2ff46f1439a830
jonathan.barroso <jony.barroso.2@gmail.com>
2022-07-05 20:19:45 -0300

    se realizaron nuevos cambios en miarchivo.js

--- miarchivo.js
+++ miarchivo.js
@@ -11,28 +11,48 @@ alert("Bienvenido a Athena.Deco, para realizar una compra debes loguearte")
 function loguearte(){
 
     let nombre = prompt("Ingrese su nombre");
+
     let email = prompt("Ingrese su mail");
+
     let contraseña = prompt("Ingrese su contraseña");
 
-    alert("Felicitaciones! haz creado con exito tu cuenta")
-    alert("Bienvenidos")
+    if(nombre && email && contraseña){
+        alert("Felicitaciones! Haz creado con exito tu cuenta, Bienvenido")
+    }else if (!nombre || !email || !contraseña){
+        alert("ud ha ingresado una forma incorrecta alguno de los datos")
+    }
+    
 }
 
-loguearte()
+
+alert("Bienvenido a Athena.Deco, para realizar una compra debes loguearte")
+
+do{
+    loguearte()
+    if(confirm("¿Desea realizar una compra?"))
+    compra()
+ }
+ while(confirm(`presione ACEPTAR para loguear un usuario o CANCELAR para salir`))
+
  
 
 function compra(){
 
     let compra = prompt("Que deseas comprar") 
     if(compra === "geometrico"){
-        alert("ud eligio geometrico");
-    }else if (compra === "perita"){
-        alert("ud eligio perita");
-    }else if( compra === "cazuela"){
-        alert("ud eligio Cazuela");   
-    }else {
+        alert("ud eligio geometrico")
         alert("fue añadido a su carrito");
+    }else if (compra === "perita"){
+        alert("ud eligio perita")
+        alert("fue añadido a su carrito");
+    }else if( compra === "cazuela"){
+        alert("ud eligio Cazuela")
+        alert("fue añadido a su carrito");   
+    }else {
+        alert("fue añadido a su carrito")
+        alert("su compra ha sido realizada con exito");
     }
 
     }
-    
+
+compra()    
